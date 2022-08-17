92c00c76122aad9d29164b4438f53c3eef3053ed
NikolayIliev87 <103068553+NikolayIliev87@users.noreply.github.com>
2022-08-17 19:51:40 +0300

    css responsive implementation with small html changes

--- tiecket-service-project/client/frontend/src/components/Home/HomeGuest.js
+++ tiecket-service-project/client/frontend/src/components/Home/HomeGuest.js
@@ -8,7 +8,7 @@ export const HomeGuest = () => {
         <div className={styles.Home}>
             <h1>Welcome to our Ticket Portal</h1>
             <h2>If you have account please <li><Link to="/login">Login</Link></li></h2>
-            <h2>Or if you still do not have please <li><Link to="/register">Register</Link></li> to continue</h2>
+            <h2>If not please <li><Link to="/register">Register</Link></li> to continue</h2>
         </div>
     )
 }
