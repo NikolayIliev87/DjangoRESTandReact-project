4132389a65737c88eef528c9686cb41e8a7bdb4b
NikolayIliev87 <103068553+NikolayIliev87@users.noreply.github.com>
2022-08-19 16:39:12 +0300

    add filters formatting of selected filter

--- tiecket-service-project/client/frontend/src/components/Tickets/Filter.js
+++ tiecket-service-project/client/frontend/src/components/Tickets/Filter.js
@@ -1,9 +1,13 @@
 import styles from './Tickets.module.css'
 
+import { useState } from 'react'
+
 export const Filter = (props) => {
+    const [currentFilter, setCurrentFilter] = useState('All')
 
     const onFilterSelectHandler = (filterID) => {
         props.onFilter(filterID)
+        setCurrentFilter(filterID)
     }
 
     return (
@@ -12,14 +16,15 @@ export const Filter = (props) => {
         <h3>Categories:</h3>
             {props.categoryList.map(category => {
             return (
-                <button key={category.id} onClick={() => onFilterSelectHandler(category.id)} >
+                <button className={currentFilter===category.id?"active":"inactive"} key={category.id} onClick={() => onFilterSelectHandler(category.id)} >
                 {category.name}
                 
                 </button>
             );
             })}
-            <button
-            onClick={() => onFilterSelectHandler('All')}
+            <button 
+                className={currentFilter==='All'?"active":"inactive"}
+                onClick={() => onFilterSelectHandler('All')}
             >
             All
             </button>
